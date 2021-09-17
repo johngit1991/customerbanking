c84dd712b84c0dcf07e6d344aa520bb446b115e7
johngit1991 <67289153+johngit1991@users.noreply.github.com>
2021-09-17 14:03:35 +0530

    Update App.java

--- src/main/java/com/banking/App.java
+++ src/main/java/com/banking/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Pradeepjjjjj!" );
+        System.out.println( "Hello World Pradeep Test3!" );
     }
 }
